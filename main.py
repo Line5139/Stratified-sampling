0b18f18c2a2eb1b14526d6e1f74c30ebdd7d3761
mohdhasyim <mohdhasyim@uiowa.edu>
2023-10-05 23:01:33 -0500

    Modify script 2 include writing data to excel file

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 #Load Excel file
-file_path = "path_file.xlsx"
-df = pd.read_excel(file_path, engine = "openpyxl")
+file_path = 'test_file.xlsx'
+df = pd.read_excel(file_path, engine = 'openpyxl')
 
 # Lable target sample size
 sample_size = 1000
 sample_df = df.groupby('Category').apply(lambda x: x.sample(int(sample_size * len(x) / len(df)))).reset_index(drop = True)
 
 # Export of sample into Excel file 
-output_file_path = 'path_file.xlsx'
-sample_df.to_excel (output_file_path, engine= 'openpyxl', index=False)
+"""output_file_path = 'path_file.xlsx'
+sample_df.to_excel (output_file_path, engine= 'openpyxl', index=False)"""
+
+#Export data back into a Excel File
+writer = pd.ExcelWriter('Summarised_test.xlsx')
+sample_df.to_excel(writer, sheet_name= 'Survey Data')
+writer.save()
+writer.close()
+
